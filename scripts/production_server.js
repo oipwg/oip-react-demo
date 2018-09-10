7ecbad01aed5ab26835fe2d5414d0e2ad53951fb
Sky Young <sky@alexandria.io>
2018-09-10 13:48:29 -0700

    use proper static dir

--- scripts/production_server.js
+++ scripts/production_server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use(express.static('dist'))
+app.use(express.static('../config/dist'))
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../config/dist/index.html')))
 
