846f67318a86315a601119c84aaf2a4d3f09ae94
Sky Young <sky@alexandria.io>
2018-09-14 17:06:19 -0700

    Add ipfs proxy to test

--- scripts/production_server.js
+++ scripts/production_server.js
@@ -1,8 +1,16 @@
 const path = require('path')
 const express = require('express')
+const proxy = require('http-proxy-middleware');
+
 const app = express()
 
 app.use(express.static(__dirname + '/../config/dist'))
+
+// Proxy the IPFS Download server to allow downloading without opening a new window.
+var ipfs_proxy = proxy("https://ipfs.oip.io/ipfs");
+
+// Add the IPFS proxy
+app.use('/ipfs', ipfs_proxy)
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../config/dist/index.html')))
 
